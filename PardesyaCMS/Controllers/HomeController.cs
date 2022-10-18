0eb3ddbfb40d44649dcfa682aedb3c97d21e5914
Darweesh Nashif <darweesh_nashif@hotmail.com>
2022-10-18 17:04:27 +0300

    Add project files.

---
+++ PardesyaCMS/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PardesyaCMS.Models;
+using PardesyaDLL;
+using System.Diagnostics;
+namespace PardesyaCMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int i = Mathematics.AddTwoNumbers(3,4);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PardesyaDLL/Mathematics.cs
@@ -0,0 +1,12 @@
+﻿namespace PardesyaDLL
+{
+    public class Mathematics
+    {
+        
+        
+        
+        // here
+        public static int AddTwoNumbers(int num1, int num2) => num1 + num2;
+    }
+    
+}
